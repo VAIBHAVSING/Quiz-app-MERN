d611d7954cc89c33ad733e1ef67dbfe410b6d97d
Vaibhav Patil <vpatil5212@gmail.com>
2024-05-11 22:47:11 +0530

    cors added to /server/index.js

--- server/index.js
+++ server/index.js
@@ -2,24 +2,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const adminRouter = require("./routes/admin");
-const userRouter=require("./routes/user");
+const userRouter = require("./routes/user");
 const cors = require('cors');
 app.use(cors());
 // const userRouter = require("./routes/user");
+app.use(cors());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods","GET, HEAD, OPTIONS, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+    22
+});
 // Middleware for parsing request bodies
 app.use(express.json());
 app.use(bodyParser.json());
 app.use("/admin", adminRouter)
-app.use('/user',userRouter)
+app.use('/user', userRouter)
 // app.use("/user", userRouter)
-app.use((err,req,res,next)=>{
-    res.json({msg:"something is wrong in server"})
+app.use((err, req, res, next) => {
+    res.json({ msg: "something is wrong in server" })
 })._router
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("<div> hii from vsing</div>")
 })
-const PORT = 3000||process.env.PORT;
+const PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
